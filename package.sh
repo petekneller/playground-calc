67b8e064d573f18f225ef77763d0d3c6cd2e89c8
petekneller <mrkneller@hotmail.com>
2014-02-22 21:46:30 +0000

    Oops, got the artifact path wrong for the one jar

--- package.sh
+++ package.sh
@@ -5,6 +5,6 @@
 version_string="version in ThisBuild := \"${BUILD_NUMBER:-dev}\""
 echo $version_string | tee build-number.sbt
 
-sbt clean test package package
+sbt clean test oneJar
 
-echo "##teamcity[publishArtifacts 'calc/target/scala-2.10/playground-calc-*-complete.jar']"
+echo "##teamcity[publishArtifacts 'calc/target/scala-2.10/playground-calc*-complete.jar']"
