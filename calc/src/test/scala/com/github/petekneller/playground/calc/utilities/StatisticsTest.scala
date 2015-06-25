5ff830ec297fa22bdac35cb07a4372f3b055582a
Pete Kneller <Peter.Kneller@barclayswealth.com>
2015-06-25 15:52:24 +0100

    Adding further stats - number of successes

--- calc/src/test/scala/com/github/petekneller/playground/calc/utilities/StatisticsTest.scala
+++ calc/src/test/scala/com/github/petekneller/playground/calc/utilities/StatisticsTest.scala
@@ -4,6 +4,9 @@ import com.github.petekneller.playground.calc.Calculator
 import com.github.petekneller.playground.calc.utilities.Statistics.Results
 import org.scalatest.{Matchers, FlatSpec}
 
+import scala.util.Random
+import scalaz.-\/
+
 class StatisticsTest extends FlatSpec with Matchers {
 
   val n = 1000
@@ -13,4 +16,12 @@ class StatisticsTest extends FlatSpec with Matchers {
     val Results(numSuccesses) = Statistics(n, Calculator.run(_))
     numSuccesses should equal(1000)
   }
+
+  it should "should report number of successes" in {
+
+    val failsHalfTheTime: Calculator = (input: String) => { if (Random.nextBoolean()) -\/("argh!") else Calculator.run(input) }
+    val Results(numSuccesses) = Statistics(n, failsHalfTheTime)
+    numSuccesses should (be > 0 and be < 1000)
+  }
+
 }
